619ff5220205767f93e52aa7846df81f0353cedc
Noel Delgado <auhcsei@gmail.com>
2020-03-06 00:43:16 -0600

    social external links

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     locals: {
+        social: require('./data/social'),
         demos: require('./data/demos'),
         oss: require('./data/oss'),
         projects: require('./data/projects')
